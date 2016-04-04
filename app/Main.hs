c8470e4e20ba8e5abec0daa0303b6b9a5198116a
Erik Lee <eriklee@gambitresearch.com>
2016-04-04 18:41:40 +0100

    much faster
    also apparently broken. fuck.
    
    uses a grid to store the visited path nodes (should maybe be unboxed,
    but that means duplicating loads of code :/)
    
    uses Value == Int isntead of Integer and only converts at the end
    
    uses unsafe array operations
    
    ---
    [0]erik@thor:~/scratch/hs/travel-agent% time
    ./.stack-work/install/x86_64-linux/lts-5.8/7.10.3/bin/travel-agent-exe >
    timed.out
    ./.stack-work/install/x86_64-linux/lts-5.8/7.10.3/bin/travel-agent-exe >
    1237.15s user 742.39s system 326% cpu 10:06.84 total

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,32 @@
 {-# LANGUAGE RecordWildCards #-}
-module Main where
+module Main
+(main) where
 
-import Lib
 import qualified Data.Vector as V
+--import qualified Data.Vector.Unboxed as VU
 import Data.Maybe
 
 main :: IO ()
-main = mapM_ printInfo runReal
+main = do --profiler
+    let paths = runReal
+    --let bestPath = maximumBy (comparing getValue) paths
+    --printInfo bestPath
+    mapM_ printInfo paths
+
+--profiler = mapM_ printInfo $ runDFS (7,7) $ State bigProfilingGrid realDie [((0,0), GVValue 1)]
+--                                                    (Grid 8 8 (V.replicate 64 False))
 
 printInfo :: State -> IO ()
-printInfo State{..} = do
-    let value = product $ map snd $ fixPathConstraints die path
+printInfo s@State{..} = do
+    let value = getValue s
     putStrLn $ "Total Value: " ++ show value
-    putStrLn $ "Total Length: " ++ show (length path)
-    print (reverse path)
+--    putStrLn $ "Total Length: " ++ show (length path)
+    --print (reverse path)
 
+getValue :: State -> Integer
+getValue State{..} = product $ map (fromIntegral . snd) $ fixPathConstraints die path
+
+fixPathConstraints :: Die -> [(t, GridValue)] -> [(t, Value)]
 fixPathConstraints die = map fixElem
   where
     fixElem (p, GVValue val) = (p, val)
@@ -25,31 +37,27 @@ runDFS :: GridPosition -> State -> [State]
 runDFS end state = go [state]
   where
     go [] = []
-    go (x:xs) = if isFinished end x
+    go (x:xs) = if isFinished x
                   then x : go (doMoves x ++ xs)
                   else go (doMoves x ++ xs)
+    isFinished State{..} = getGridCell pathGrid end
 
-isFinished end State{..} = getPosFromPath path == end
+--isFinished end State{..} = getPosFromPath path == end
 
 --- Data
+runReal :: [State]
 runReal = runDFS (11,11) realState
-runAnswer = runDFS (11,11) $ State realGrid answerDie [((0,0), GVValue 1)]
+--runAnswer = runDFS (11,11) $ State realGrid answerDie [((0,0), GVValue 1)]
 
-testDie = Die DfA DfC [(DfA, 1), (DfB, 2), (DfC, 3), (DfD, 4)]
-testGrid = Grid 2 2 (V.fromList [Just 1,Just 2,Nothing,Just 3])
-testState = State testGrid testDie [((0,0), GVValue 1)]
+--testDie = Die DfA DfC [(DfA, 1), (DfB, 2), (DfC, 3), (DfD, 4)]
+--testGrid = Grid 2 2 (V.fromList [Just 1,Just 2,Nothing,Just 3])
+--testState = State testGrid testDie [((0,0), GVValue 1)]
 
-bigTestGrid = Grid 5 5 (V.fromList (Just <$> [3,4,1,7,5, 1,2,4,3,5, 2,4,3,6,2, 9,5,7,2,3, 5,8,3,4,1]))
-bigTestDie = Die DfA DfC [(DfA, 3),(DfB, 4), (DfC, 4), (DfD, 8), (DfE, 5), (DfF, 1)]
-bigTestState = State bigTestGrid bigTestDie [((0,0), GVValue 3)]
+--bigTestGrid = Grid 5 5 (V.fromList (Just <$> [3,4,1,7,5, 1,2,4,3,5, 2,4,3,6,2, 9,5,7,2,3, 5,8,3,4,1]))
+--bigTestDie = Die DfA DfC $ FV (Just 3) (Just 4) (Just 4) (Just 8) (Just 5) (Just 1)
+--bigTestState = State bigTestGrid bigTestDie [((0,0), GVValue 3)] $ Grid 5 5 (V.replicate 25 False)
 
-bigTestDieEmpties = Die DfA DfC [(DfA, 3),(DfB, 4), (DfC, 4), (DfE, 5), (DfF, 1)]
-bigTestGridWEmpties = Grid 5 5 (V.fromList
-  [Just 3,Just 4, Just 1, Just 7,Just 5,
-   Just 1,Just 2, Just 4, Just 3,Just 5,
-   Just 2,Just 4,Nothing, Just 6,Just 2,
-   Just 9,Nothing,Just 7, Just 2,Just 3,
-   Just 5,Just 8, Just 3, Just 4,Just 1])
+realGrid :: Grid (Maybe Value)
 realGrid = Grid 12 12 (V.fromList
   [ Just 1, Just 5, Just 4, Just 4, Just 6, Just 1, Just 1, Just 4, Just 1, Just 3, Just 7, Just 5,
     Just 3,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing, Just 1,
@@ -64,12 +72,26 @@ realGrid = Grid 12 12 (V.fromList
     Just 6,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing, Just 3,
     Just 2, Just 1, Just 6, Just 6, Just 4, Just 5, Just 2, Just 1, Just 1, Just 1, Just 7, Just 1])
 
-realDie = Die DfA DfC [(DfA, 1)]
-answerDie = Die DfA DfC [(DfA, 1), (DfB, 7), (DfC, 3), (DfD, 8), (DfE, 6), (DfF, 6)]
-realState = State realGrid realDie [((0,0), GVValue 1)]
+{-- bigProfilingGrid :: Grid (Maybe Value)
+bigProfilingGrid = Grid 8 8 (V.fromList
+  [ Just 6, Just 4, Just 1, Just 8, Just 1, Just 4, Just 2, Just 1,
+    Just 1,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing, Just 1,
+    Just 1,Nothing, Just 6, Just 1, Just 6, Just 2,Nothing, Just 2,
+    Just 4,Nothing, Just 1,Nothing,Nothing, Just 8,Nothing, Just 3,
+    Just 2,Nothing, Just 5,Nothing,Nothing, Just 3,Nothing, Just 5,
+    Just 5,Nothing, Just 1, Just 1, Just 2, Just 3,Nothing, Just 4,
+    Just 1,Nothing,Nothing,Nothing,Nothing,Nothing,Nothing, Just 3,
+    Just 6, Just 3, Just 6, Just 5, Just 4, Just 3, Just 4, Just 5]) --}
+
+realDie :: Die
+realDie = Die DfA DfC $ FV (Just 1) Nothing Nothing Nothing Nothing Nothing
+--answerDie = Die DfA DfC [(DfA, 1), (DfB, 7), (DfC, 3), (DfD, 8), (DfE, 6), (DfF, 6)]
+realState :: State
+realState = State realGrid realDie [((0,0), GVValue 1)] $ Grid 12 12 (V.replicate 144 False)
+
 
 --- Die Definitions
-type Value = Integer -- (1-9), but will be multiplied together at the end
+type Value = Int -- (1-9), but will be multiplied together at the end
 data Direction = North | South | East | West deriving (Show, Eq)
 
 data DieFace = DfA | DfB | DfC | DfD | DfE | DfF deriving (Show, Eq)
@@ -86,25 +108,47 @@ data DieFace = DfA | DfB | DfC | DfD | DfE | DfF deriving (Show, Eq)
 data Die = Die
             { topFace :: DieFace -- If this is A in the die above
             , northFace :: DieFace  -- Then this is C (gives the orientation of the die)
-            , faceValues :: [(DieFace, Value)]
+            , faceValues :: FaceValues
             } deriving (Show)
+
+data FaceValues = FV {
+      fvA :: Maybe Value
+    , fvB :: Maybe Value
+    , fvC :: Maybe Value
+    , fvD :: Maybe Value
+    , fvE :: Maybe Value
+    , fvF :: Maybe Value
+    } deriving (Show)
 
 addFaceValueToDie :: Die -> Maybe Value -> Die
 addFaceValueToDie d@Die{..} Nothing  = d
 addFaceValueToDie d@Die{..} (Just x) = d { faceValues = faceValues' }
   where
     faceValues' = case getDieValue d of
-                    Nothing -> (topFace, x) : faceValues
+                    Nothing -> updateFV faceValues topFace x
                     Just _  -> faceValues
+
+updateFV :: FaceValues -> DieFace -> Value -> FaceValues
+updateFV fv@FV{..} DfA x = fv { fvA = Just x }
+updateFV fv@FV{..} DfB x = fv { fvB = Just x }
+updateFV fv@FV{..} DfC x = fv { fvC = Just x }
+updateFV fv@FV{..} DfD x = fv { fvD = Just x }
+updateFV fv@FV{..} DfE x = fv { fvE = Just x }
+updateFV fv@FV{..} DfF x = fv { fvF = Just x }
+
 
 getDieValue :: Die -> Maybe Value
 getDieValue d@Die{..} = getDieFaceValue d topFace
 
 getDieFaceValue :: Die -> DieFace -> Maybe Value
-getDieFaceValue Die{..} face =
-    case filter (\v -> face == fst v) faceValues of
-        [] -> Nothing
-        [(top, val)] -> Just val
+getDieFaceValue Die{..} = getDieFaceValue' faceValues
+  where
+    getDieFaceValue' (FV x _ _ _ _ _) DfA = x
+    getDieFaceValue' (FV _ x _ _ _ _) DfB = x
+    getDieFaceValue' (FV _ _ x _ _ _) DfC = x
+    getDieFaceValue' (FV _ _ _ x _ _) DfD = x
+    getDieFaceValue' (FV _ _ _ _ x _) DfE = x
+    getDieFaceValue' (FV _ _ _ _ _ x) DfF = x
 
 roll :: Die -> Direction -> Die
 roll die dir = let (top, north) = roll' dir (topFace die) (northFace die) in
@@ -117,6 +161,7 @@ roll' West  top north = let rtop = dieRight' top north in (opp rtop, north)
 roll' South top north = (north, opp top)
 roll' East  top north = (dieRight' top north, north)
 
+dieRight' :: DieFace -> DieFace -> DieFace
 dieRight' DfA DfB = DfE
 dieRight' DfA DfC = DfB
 dieRight' DfA DfD = DfC
@@ -159,25 +204,29 @@ type GridPosition = (Int, Int) -- (x,y), top left is (0,0)
 data GridValue = GVValue Value | GVConstraint DieFace
     deriving (Show)
 
-data Grid = Grid
+data Grid a = Grid
             { width :: Int
             , height :: Int
-            , cells :: V.Vector (Maybe Value)
+            , cells :: V.Vector a
             } deriving (Show)
 
+{--
 printGrid Grid{..} = go (V.toList cells)
   where
     go [] = putStrLn ""
     go cs = let (row, rems) = splitAt width cs in print row >> go rems
+    --}
 
 type Path = [(GridPosition, GridValue)]
 
+getPosFromPath :: Path -> (GridPosition)
 getPosFromPath = fst . head
 
 data State = State
-            { grid :: Grid
+            { grid :: Grid (Maybe Value)
             , die :: Die
             , path :: Path
+            , pathGrid :: Grid Bool
             } deriving (Show)
 
 doMoves :: State -> [State]
@@ -187,14 +236,14 @@ doMoves s@State{..} =
     -- check that grid doesn't block move
     dirs = gridDirections grid pos
     -- check that path doesn't block move (i.e repeating a space)
-    dirs' = filter (pathAllows path) dirs
+    dirs' = filter (pathAllows pathGrid pos) dirs
     -- check that the die allows a move
     dirs'' = filter (dieAllows grid pos die) dirs' in
   -- actually move/rotate die, add to path
   map (moveDie s) dirs''
 
 moveDie :: State -> Direction -> State
-moveDie s@State{..} dir = s{die=die'', path=path'}
+moveDie s@State{..} dir = s{die=die'', path=path', pathGrid=pathGrid'}
   where
     pos = getPosFromPath path
     pos' = changePos pos dir
@@ -202,9 +251,13 @@ moveDie s@State{..} dir = s{die=die'', path=path'}
     gv = getGridCell grid pos'
     die'' = addFaceValueToDie die' gv
     path' = addMoveToPath pos' die'' path
+    pathGrid' = addMoveToPathGrid pathGrid pos'
 
 addMoveToPath :: GridPosition -> Die -> Path -> Path
 addMoveToPath pos die path = (pos, valOrFace die) : path
+
+addMoveToPathGrid :: Grid Bool -> GridPosition -> Grid Bool
+addMoveToPathGrid path@Grid{..} pos = path { cells = cells `V.unsafeUpd` [(getGridIndex path pos, True)] }
 
 valOrFace :: Die -> GridValue
 valOrFace d@Die{..} =
@@ -212,7 +265,7 @@ valOrFace d@Die{..} =
         Nothing  -> GVConstraint topFace
         Just val -> GVValue val
 
-gridDirections :: Grid -> GridPosition -> [Direction]
+gridDirections :: Grid a -> GridPosition -> [Direction]
 gridDirections grid (x,y) = catMaybes [checkN, checkS, checkE, checkW]
     where
         checkN = if y > 0 then Just North else Nothing
@@ -220,19 +273,19 @@ gridDirections grid (x,y) = catMaybes [checkN, checkS, checkE, checkW]
         checkS = if y < (height grid - 1) then Just South else Nothing
         checkE = if x < (width grid - 1) then Just East else Nothing
 
-pathAllows :: Path -> Direction -> Bool
-pathAllows path dir = pos' `notElem` (fst <$> path)
+pathAllows :: Grid Bool -> GridPosition -> Direction -> Bool
+pathAllows pathGrid pos dir = not $ getGridCell pathGrid pos'
   where
-    pos = getPosFromPath path
     pos' = changePos pos dir
 
+changePos :: GridPosition -> Direction -> GridPosition
 changePos (x,y) North = (x, y-1)
 changePos (x,y) South = (x, y+1)
 changePos (x,y) East  = (x+1, y)
 changePos (x,y) West  = (x-1, y)
 
 -- Either one of the values can be Nothing, or they must be equal
-dieAllows :: Grid -> GridPosition -> Die -> Direction -> Bool
+dieAllows :: Grid (Maybe Value) -> GridPosition -> Die -> Direction -> Bool
 dieAllows grid pos die dir = isNothing dv || isNothing gc || dv == gc
   where
     pos' = changePos pos dir
@@ -240,5 +293,8 @@ dieAllows grid pos die dir = isNothing dv || isNothing gc || dv == gc
     dv   = getDieValue die'
     gc   = getGridCell grid pos'
 
-getGridCell :: Grid -> GridPosition -> Maybe Value
-getGridCell Grid{..} (x,y) = cells V.! (x + y * width)
+getGridCell :: Grid a -> GridPosition -> a
+getGridCell g@Grid{..} pos = cells `V.unsafeIndex` getGridIndex g pos
+
+getGridIndex :: Grid a -> (Int, Int) -> Int
+getGridIndex Grid{..} (x,y) = x + y * width
