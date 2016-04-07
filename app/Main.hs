7e0d509fdf603ace7317b6a3b3b8cd5ea8630a31
Erik Lee <eriklee@gambitresearch.com>
2016-04-07 11:20:42 +0100

    Maybe Int -> Int and 0 == Nothing
    It's pretty gross, and I don't feel good about it, but it does take 3.3
    seconds, so...

--- app/Main.hs
+++ app/Main.hs
@@ -34,20 +34,20 @@ positionInGrid :: Grid a -> GridPosition -> Bool
 positionInGrid Grid{..} (x,y) =
       x >= 0 && y >= 0 && x < width && y < height
 
-notVisited :: Grid (Maybe a) -> GridPosition -> Bool
-notVisited g p = isNothing $ getGridValue g p
+notVisited :: (Eq a, Num a) => Grid a -> GridPosition -> Bool
+notVisited g p = (== 0) $ getGridValue g p
 
 getGridValue :: Grid a -> GridPosition -> a
 getGridValue Grid{..} (x,y) = cells `V.unsafeIndex` (y * width + x)
 
-validMoves :: Grid (Maybe a) -> GridPosition -> [GridPosition]
+validMoves :: (Eq a, Num a) => Grid a -> GridPosition -> [GridPosition]
 validMoves grid = filter (notVisited grid) . filter (positionInGrid grid) . nextMoves
 
 foldGridRow :: (b -> a -> b) -> b -> Grid a -> Int -> b
-foldGridRow f init grid n = foldl f init (getRow grid n)
+foldGridRow f init grid n = V.foldl' f init (getRowV grid n)
 
-getRow :: Grid a -> Int -> [a]
-getRow Grid{..} n = V.toList $ V.unsafeSlice (n * height) width cells
+getRowV :: Grid a -> Int -> V.Vector a
+getRowV Grid{..} n = V.unsafeSlice (n * height) width cells
 --[ cells V.! x | x <- [n * height .. n * height + width - 1] ]
 
 foldGridCol :: (b -> a -> b) -> b -> Grid a -> Int -> b
@@ -57,23 +57,23 @@ getCol :: Grid a -> Int -> [a]
 getCol Grid{..} n = [ cells `V.unsafeIndex` (n + (x * width)) | x <- [0..height - 1] ]
 
 initialGrid = Grid 8 8 $ V.fromList
-    [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing,
-     Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing,
-     Nothing, Nothing, Nothing, Just 11, Nothing, Nothing, Nothing, Nothing,
-     Nothing, Nothing, Nothing, Nothing, Nothing, Just 14, Nothing, Nothing,
-     Nothing, Nothing, Just  8, Nothing, Nothing, Nothing, Nothing, Nothing,
-     Nothing, Nothing, Nothing, Nothing, Just 15, Nothing, Nothing, Nothing,
-     Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing,
-     Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]
+    [ 0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,
+      0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,
+      0 ,  0 ,  0 , 11 ,  0 ,  0 ,  0 ,  0 ,
+      0 ,  0 ,  0 ,  0 ,  0 , 14 ,  0 ,  0 ,
+      0 ,  0 ,  8 ,  0 ,  0 ,  0 ,  0 ,  0 ,
+      0 ,  0 ,  0 ,  0 , 15 ,  0 ,  0 ,  0 ,
+      0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,
+      0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ]
 
-enumerateMoves :: (Grid (Maybe Int) -> Bool) -> Grid (Maybe Int) -> GridPosition -> (Int -> Maybe Int) -> Maybe Int -> [Grid (Maybe Int)]
+enumerateMoves :: (Grid Int -> Bool) -> Grid Int -> GridPosition -> (Int -> Maybe Int) -> Maybe Int -> [Grid Int]
 --               starting grid   -> starting pos ->  next label   -> initial label -> grids
 enumerateMoves filterF grid pos labelIter Nothing  = [grid]
 enumerateMoves filterF grid pos labelIter (Just x) = concatMap mapFun nextGrids
     where
         mapFun (g, pos) = enumerateMoves filterF g pos labelIter nextLabel
         nextLabel = labelIter x
-        nextGrids = filter (filterF . fst) $ map (\p -> (labelGridCell grid p (Just x), p)) (validMoves grid pos)
+        nextGrids = filter (filterF . fst) $ map (\p -> (labelGridCell grid p x, p)) (validMoves grid pos)
 
 labelGridCell :: Grid a -> GridPosition -> a -> Grid a
 labelGridCell g@Grid{..} pos x = g { cells = cells `V.unsafeUpd` [(index, x)] }
@@ -85,27 +85,27 @@ gridIndex Grid{..} (x,y) = x + (width * y)
 vecIndexToPos :: Grid a -> Int -> GridPosition
 vecIndexToPos Grid{..} n = (n `rem` width, n `div` height)
 
-lowerGrids :: [Grid (Maybe Int)]
+lowerGrids :: [Grid Int]
 lowerGrids = enumerateMoves (const True) initialGrid (2,4) (\x -> if x == 1 then Nothing else Just (x - 1)) (Just 7)
 
 -- Takes a grid and tries to find knight moves from the 8 square that will be compatible with the 11
-eightTo11 :: Grid (Maybe Int) -> [Grid (Maybe Int)]
+eightTo11 :: Grid Int -> [Grid Int]
 eightTo11 g = filter compatibleWith11 potentials
   where
     potentials = enumerateMoves (const True)g (2,4) (\x -> if x == 10 then Nothing else Just (x + 1)) (Just 9)
-    get10Pos g = vecIndexToPos g . fromJust $ Just 10 `V.elemIndex` cells g
+    get10Pos g = vecIndexToPos g . fromJust $ 10 `V.elemIndex` cells g
     compatibleWith11 g = eleven `elem` nextMoves (get10Pos g)
     eleven = (3,2)
 
-elevenTo14 :: Grid (Maybe Int) -> [Grid (Maybe Int)]
+elevenTo14 :: Grid Int -> [Grid Int]
 elevenTo14 g = filter compatibleWith14 potentials
   where
     potentials = enumerateMoves (const True) g (3,2) (\x -> if x == 13 then Nothing else Just (x + 1)) (Just 12)
-    get13Pos g = vecIndexToPos g . fromJust $ Just 13 `V.elemIndex` cells g
+    get13Pos g = vecIndexToPos g . fromJust $ 13 `V.elemIndex` cells g
     compatibleWith14 g = fourteen `elem` nextMoves (get13Pos g)
     fourteen = (5,3)
 
-sixteenTo28 :: Grid (Maybe Int) -> [Grid (Maybe Int)]
+sixteenTo28 :: Grid Int -> [Grid Int]
 sixteenTo28 g = potentials
   where
     potentials = enumerateMoves filterBadSums g (4,5) (\x -> if x == 28 then Nothing else Just (x + 1)) (Just 16)
@@ -114,8 +114,8 @@ rowSums, colSums :: [Int]
 rowSums = [10, 34, 108, 67, 63, 84, 24, 16]
 colSums = [7, 14, 72, 66, 102, 90, 42, 13]
 
-getGridRowSums g = map (sum . catMaybes . getRow g) [0..7]
-getGridColSums g = map (sum . catMaybes . getCol g) [0..7]
+getGridRowSums g = map (V.foldl1' (+) . getRowV g) [0..7]
+getGridColSums g = map (sum . getCol g) [0..7]
 
 filterBadSums g = and (zipWith (<=) (getGridRowSums g) rowSums) &&
                   and (zipWith (<=) (getGridColSums g) colSums)
@@ -127,19 +127,21 @@ elevenFilter g = head (getGridRowSums g) == 10 && take 2 (getGridColSums g) == [
 
 solutions = filter lowFilter lowerGrids >>= filter elevenFilter . eightTo11 >>= filter elevenFilter . elevenTo14 >>= sixteenTo28
 
-maxProduct g = maximum $ map (product . catMaybes . getRow g) [0..7] ++
-                         map (product . catMaybes . getCol g) [0..7]
+maxProduct g = maximum $ map (V.foldl' nonZeroProd 1 . getRowV g) [0..7] ++
+                         map (foldl nonZeroProd 1 . getCol g) [0..7]
 
+nonZeroProd acc 0 = acc
+nonZeroProd acc n = acc * n
 -- The final grid obtained from the above. Takes a minute or two to find. (max = 19675656)
 finalGrid = Grid 8 8 $ V.fromList . concat $
-   [[Nothing ,Nothing ,Just 10, Nothing, Nothing, Nothing, Nothing, Nothing]
-   ,[Nothing ,Nothing ,Nothing, Just 22, Nothing, Just 12, Nothing, Nothing]
-   ,[Nothing , Just 9 ,Just 28, Just 11, Just 26, Just 21, Nothing, Just 13]
-   ,[Nothing ,Nothing , Just 1,  Just 4, Just 23, Just 14, Just 25, Nothing]
-   ,[Nothing , Just 5 , Just 8, Just 27, Just 20, Just 3 , Nothing, Nothing]
-   ,[Just 7  ,Nothing ,Just 19,  Just 2, Just 15, Just 24, Just 17, Nothing]
-   ,[Nothing ,Nothing , Just 6, Nothing, Just 18, Nothing, Nothing, Nothing]
-   ,[Nothing ,Nothing ,Nothing, Nothing, Nothing, Just 16, Nothing, Nothing]]
+   [[ 0 , 0 , 10, 0 ,  0 , 0 , 0 , 0]
+   ,[ 0 , 0 , 0 , 22,  0 , 12, 0 , 0]
+   ,[ 0 , 9 , 28, 11,  26, 21, 0 , 13]
+   ,[ 0 , 0 , 1 , 4 ,  23, 14, 25, 0]
+   ,[ 0 , 5 , 8 , 27,  20, 3 , 0 , 0]
+   ,[ 7  ,0 , 19, 2 ,  15, 24, 17, 0]
+   ,[ 0 , 0 , 6 , 0 ,  18, 0 , 0 , 0]
+   ,[ 0 , 0 , 0 , 0 ,  0 , 16, 0 , 0]]
 
 output g = do
     putStrLn "Solution found:"
