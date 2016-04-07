90dc0baa701512b14c24913bbe477d4d48b41009
Erik Lee <eriklee@gambitresearch.com>
2016-04-07 10:42:33 +0100

    This might be faster

--- app/Main.hs
+++ app/Main.hs
@@ -38,7 +38,7 @@ notVisited :: Grid (Maybe a) -> GridPosition -> Bool
 notVisited g p = isNothing $ getGridValue g p
 
 getGridValue :: Grid a -> GridPosition -> a
-getGridValue Grid{..} (x,y) = cells V.! (y * width + x)
+getGridValue Grid{..} (x,y) = cells V.unsafeIndex (y * width + x)
 
 validMoves :: Grid (Maybe a) -> GridPosition -> [GridPosition]
 validMoves grid = filter (notVisited grid) . filter (positionInGrid grid) . nextMoves
@@ -47,13 +47,14 @@ foldGridRow :: (b -> a -> b) -> b -> Grid a -> Int -> b
 foldGridRow f init grid n = foldl f init (getRow grid n)
 
 getRow :: Grid a -> Int -> [a]
-getRow Grid{..} n = [ cells V.! x | x <- [n * height .. n * height + width - 1] ]
+getRow Grid{..} n = V.toList $ V.unsafeSlice (n * height) width cells
+--[ cells V.! x | x <- [n * height .. n * height + width - 1] ]
 
 foldGridCol :: (b -> a -> b) -> b -> Grid a -> Int -> b
 foldGridCol f init grid n = foldl f init (getCol grid n)
 
 getCol :: Grid a -> Int -> [a]
-getCol Grid{..} n = [ cells V.! (n + (x * width)) | x <- [0..height - 1] ]
+getCol Grid{..} n = [ cells V.unsafeIndex (n + (x * width)) | x <- [0..height - 1] ]
 
 initialGrid = Grid 8 8 $ V.fromList
     [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing,
@@ -65,17 +66,17 @@ initialGrid = Grid 8 8 $ V.fromList
      Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing,
      Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]
 
-enumerateMoves :: Grid (Maybe Int) -> GridPosition -> (Int -> Maybe Int) -> Maybe Int -> [Grid (Maybe Int)]
+enumerateMoves :: (Grid (Maybe Int) -> Bool) -> Grid (Maybe Int) -> GridPosition -> (Int -> Maybe Int) -> Maybe Int -> [Grid (Maybe Int)]
 --               starting grid   -> starting pos ->  next label   -> initial label -> grids
-enumerateMoves grid pos labelIter Nothing  = [grid]
-enumerateMoves grid pos labelIter (Just x) = concatMap mapFun nextGrids
+enumerateMoves filterF grid pos labelIter Nothing  = [grid]
+enumerateMoves filterF grid pos labelIter (Just x) = concatMap mapFun nextGrids
     where
-        mapFun (g, pos) = enumerateMoves g pos labelIter nextLabel
+        mapFun (g, pos) = enumerateMoves filterF g pos labelIter nextLabel
         nextLabel = labelIter x
-        nextGrids = filter (filterBadSums . fst) $ map (\p -> (labelGridCell grid p (Just x), p)) (validMoves grid pos)
+        nextGrids = filter (filterF . fst) $ map (\p -> (labelGridCell grid p (Just x), p)) (validMoves grid pos)
 
 labelGridCell :: Grid a -> GridPosition -> a -> Grid a
-labelGridCell g@Grid{..} pos x = g { cells = cells V.// [(index, x)] }
+labelGridCell g@Grid{..} pos x = g { cells = cells V.unsafeUpd [(index, x)] }
     where index = gridIndex g pos
 
 gridIndex :: Grid a -> GridPosition -> Int
@@ -85,13 +86,13 @@ vecIndexToPos :: Grid a -> Int -> GridPosition
 vecIndexToPos Grid{..} n = (n `rem` width, n `div` height)
 
 lowerGrids :: [Grid (Maybe Int)]
-lowerGrids = enumerateMoves initialGrid (2,4) (\x -> if x == 1 then Nothing else Just (x - 1)) (Just 7)
+lowerGrids = enumerateMoves (const True) initialGrid (2,4) (\x -> if x == 1 then Nothing else Just (x - 1)) (Just 7)
 
 -- Takes a grid and tries to find knight moves from the 8 square that will be compatible with the 11
 eightTo11 :: Grid (Maybe Int) -> [Grid (Maybe Int)]
 eightTo11 g = filter compatibleWith11 potentials
   where
-    potentials = enumerateMoves g (2,4) (\x -> if x == 10 then Nothing else Just (x + 1)) (Just 9)
+    potentials = enumerateMoves (const True)g (2,4) (\x -> if x == 10 then Nothing else Just (x + 1)) (Just 9)
     get10Pos g = vecIndexToPos g . fromJust $ Just 10 `V.elemIndex` cells g
     compatibleWith11 g = eleven `elem` nextMoves (get10Pos g)
     eleven = (3,2)
@@ -99,7 +100,7 @@ eightTo11 g = filter compatibleWith11 potentials
 elevenTo14 :: Grid (Maybe Int) -> [Grid (Maybe Int)]
 elevenTo14 g = filter compatibleWith14 potentials
   where
-    potentials = enumerateMoves g (3,2) (\x -> if x == 13 then Nothing else Just (x + 1)) (Just 12)
+    potentials = enumerateMoves (const True) g (3,2) (\x -> if x == 13 then Nothing else Just (x + 1)) (Just 12)
     get13Pos g = vecIndexToPos g . fromJust $ Just 13 `V.elemIndex` cells g
     compatibleWith14 g = fourteen `elem` nextMoves (get13Pos g)
     fourteen = (5,3)
@@ -107,7 +108,7 @@ elevenTo14 g = filter compatibleWith14 potentials
 sixteenTo28 :: Grid (Maybe Int) -> [Grid (Maybe Int)]
 sixteenTo28 g = potentials
   where
-    potentials = enumerateMoves g (4,5) (\x -> if x == 28 then Nothing else Just (x + 1)) (Just 16)
+    potentials = enumerateMoves filterBadSums g (4,5) (\x -> if x == 28 then Nothing else Just (x + 1)) (Just 16)
 
 rowSums, colSums :: [Int]
 rowSums = [10, 34, 108, 67, 63, 84, 24, 16]
