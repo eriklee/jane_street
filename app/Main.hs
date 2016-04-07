db7f3ae486159cad57746cd1633f2d63b83cc212
Erik Lee <eriklee@gambitresearch.com>
2016-04-07 11:35:59 +0100

    unboxed vectors don't make a huge difference...

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-module Main where
+module Main (main) where
 
 import Lib
-import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as V
 import Data.Maybe
 
 main :: IO ()
@@ -34,28 +34,29 @@ positionInGrid :: Grid a -> GridPosition -> Bool
 positionInGrid Grid{..} (x,y) =
       x >= 0 && y >= 0 && x < width && y < height
 
-notVisited :: (Eq a, Num a) => Grid a -> GridPosition -> Bool
+--notVisited :: (Eq a, Num a) => Grid a -> GridPosition -> Bool
 notVisited g p = (== 0) $ getGridValue g p
 
-getGridValue :: Grid a -> GridPosition -> a
+--getGridValue :: Grid a -> GridPosition -> a
 getGridValue Grid{..} (x,y) = cells `V.unsafeIndex` (y * width + x)
 
-validMoves :: (Eq a, Num a) => Grid a -> GridPosition -> [GridPosition]
+--validMoves :: (Eq a, Num a) => Grid a -> GridPosition -> [GridPosition]
 validMoves grid = filter (notVisited grid) . filter (positionInGrid grid) . nextMoves
 
-foldGridRow :: (b -> a -> b) -> b -> Grid a -> Int -> b
+--foldGridRow :: (b -> a -> b) -> b -> Grid a -> Int -> b
 foldGridRow f init grid n = V.foldl' f init (getRowV grid n)
 
-getRowV :: Grid a -> Int -> V.Vector a
+--getRowV :: Grid a -> Int -> V.Vector a
 getRowV Grid{..} n = V.unsafeSlice (n * height) width cells
 --[ cells V.! x | x <- [n * height .. n * height + width - 1] ]
 
-foldGridCol :: (b -> a -> b) -> b -> Grid a -> Int -> b
+--foldGridCol :: (b -> a -> b) -> b -> Grid a -> Int -> b
 foldGridCol f init grid n = foldl f init (getCol grid n)
 
-getCol :: Grid a -> Int -> [a]
+--getCol :: Grid a -> Int -> [a]
 getCol Grid{..} n = [ cells `V.unsafeIndex` (n + (x * width)) | x <- [0..height - 1] ]
 
+initialGrid :: Grid Int
 initialGrid = Grid 8 8 $ V.fromList
     [ 0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,
       0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,
@@ -75,11 +76,11 @@ enumerateMoves filterF grid pos labelIter (Just x) = concatMap mapFun nextGrids
         nextLabel = labelIter x
         nextGrids = filter (filterF . fst) $ map (\p -> (labelGridCell grid p x, p)) (validMoves grid pos)
 
-labelGridCell :: Grid a -> GridPosition -> a -> Grid a
+--labelGridCell :: Grid a -> GridPosition -> a -> Grid a
 labelGridCell g@Grid{..} pos x = g { cells = cells `V.unsafeUpd` [(index, x)] }
     where index = gridIndex g pos
 
-gridIndex :: Grid a -> GridPosition -> Int
+--gridIndex :: Grid a -> GridPosition -> Int
 gridIndex Grid{..} (x,y) = x + (width * y)
 
 vecIndexToPos :: Grid a -> Int -> GridPosition
@@ -114,7 +115,7 @@ rowSums, colSums :: [Int]
 rowSums = [10, 34, 108, 67, 63, 84, 24, 16]
 colSums = [7, 14, 72, 66, 102, 90, 42, 13]
 
-getGridRowSums g = map (V.foldl1' (+) . getRowV g) [0..7]
+getGridRowSums g = map (V.sum . getRowV g) [0..7]
 getGridColSums g = map (sum . getCol g) [0..7]
 
 filterBadSums g = and (zipWith (<=) (getGridRowSums g) rowSums) &&
@@ -123,7 +124,7 @@ filterBadSums g = and (zipWith (<=) (getGridRowSums g) rowSums) &&
 -- applies to lower and requires we have at least the first column finished
 lowFilter g = head (getGridColSums g) == 7
 
-elevenFilter g = head (getGridRowSums g) == 10 && take 2 (getGridColSums g) == [7, 14]
+elevenFilter g = V.sum (getRowV g 0) == 10 && take 2 (getGridColSums g) == [7, 14]
 
 solutions = filter lowFilter lowerGrids >>= filter elevenFilter . eightTo11 >>= filter elevenFilter . elevenTo14 >>= sixteenTo28
 
@@ -133,6 +134,7 @@ maxProduct g = maximum $ map (V.foldl' nonZeroProd 1 . getRowV g) [0..7] ++
 nonZeroProd acc 0 = acc
 nonZeroProd acc n = acc * n
 -- The final grid obtained from the above. Takes a minute or two to find. (max = 19675656)
+finalGrid :: Grid Int
 finalGrid = Grid 8 8 $ V.fromList . concat $
    [[ 0 , 0 , 10, 0 ,  0 , 0 , 0 , 0]
    ,[ 0 , 0 , 0 , 22,  0 , 12, 0 , 0]
